eb1fed43f17f10d544ad41f3f2b683fe91427f0c
Satyam Kumar <krsatyam0605@gmail.com>
2026-08-05 11:10:51 +0530

    Time: 1750 ms (25.48%), Space: 35.2 MB (30.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,20 +3,21 @@ from typing import List
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         arr=nums
+        n=len(nums)
         t=sum(nums)
         if t%2!=0:
             return False
-        t=t//2   
-        n=len(arr)
-        dp=[[0]*(t+1) for _ in range(n+1)]
+        t=t//2
+        dp=[[False]*(t+1) for _ in range(n+1)]
+        for i in range(n + 1):
+            dp[i][0] = True
+
+        for j in range(1, t + 1):
+            dp[0][j] = False
         for i in range(1,n+1):
             for j in range(1,t+1):
                 if arr[i-1]<=j:
-                    dp[i][j]=max(arr[i-1]+dp[i-1][j-arr[i-1]],dp[i-1][j])
+                    dp[i][j]=dp[i-1][j-arr[i-1]] or dp[i-1][j]
                 else:
                     dp[i][j]=dp[i-1][j]
-        return dp[n][t]==t                
-
-
-                
-       
+        return dp[n][t]                    
