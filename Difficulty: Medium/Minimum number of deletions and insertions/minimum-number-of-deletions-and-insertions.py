71e279a8cf14c1b83d3fa073b80a99351e694619
Satyam Kumar <krsatyam0605@gmail.com>
2026-08-08 06:45:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.py
@@ -0,0 +1,22 @@
+class Solution:
+	def minOperations(self, s1, s2):
+		# code here
+		n=len(s1)
+		m=len(s2)
+		dp=[[0]*(m+1) for _ in range(n+1)]
+		
+		for i in range(1,n+1):
+		    for j in range(1,m+1):
+		        if s1[i-1]==s2[j-1]:
+		            dp[i][j]=1+dp[i-1][j-1]
+		        else:
+		            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+		            
+		            
+		            
+		            
+		lcs=dp[n][m]
+		de=n-lcs
+		ins=m-lcs
+		return de+ins
+		
